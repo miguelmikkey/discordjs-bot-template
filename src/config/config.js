f0fa00a11b0906e84868611b2c47e8a818256ba7
miguelmikkey <mangelgonzalezmartinez@gmail.com>
2025-03-19 14:02:04 +0000

    Removed GUILD_ID from config.js file

--- src/config/config.js
+++ src/config/config.js
@@ -2,13 +2,12 @@ require("dotenv").config();
 const { ActivityType } = require("discord.js");
 
 module.exports = {
-  GUILD_ID: process.env.GUILD_ID,
   BOT_TOKEN: process.env.DISCORD_TOKEN,
   presence: {
     status: "dnd",
     activities: [
       {
-        name: "discord-bot-template",
+        name: "discordjs-bot-template",
         type: ActivityType.Playing,
       },
     ],
